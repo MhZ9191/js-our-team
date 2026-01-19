695d093b88c12726c731078179832ffd3a76f0f5
MhZ9191 <hegic91@gmail.com>
2026-01-19 17:47:16 +0100

    Added improvements

--- JS/function.js
+++ JS/function.js
@@ -1,9 +1,48 @@
-function addCard(divMain) {
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png",
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png",
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png",
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png",
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png",
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png",
+  },
+];
+
+function addCards(container) {
   let tmp = "";
   for (const teamMember of teamMembers) {
     tmp += AddMember(teamMember);
   }
-  divMain.innerHTML = tmp;
+  container.innerHTML = tmp;
 }
 
 function AddMember({ name, role, email, img }) {
@@ -39,8 +78,7 @@ function send(e) {
     img: immagine.value,
   };
   teamMembers.push(obj);
-  AddMember(obj);
-  addCard(divMain);
+  addCards(container);
   nome.value = "";
   ruolo.value = "";
   mail.value = "";
--- JS/script.js
+++ JS/script.js
@@ -1,44 +1,5 @@
-const teamMembers = [
-  {
-    name: "Marco Bianchi",
-    role: "Designer",
-    email: "marcobianchi@team.com",
-    img: "img/male1.png",
-  },
-  {
-    name: "Laura Rossi",
-    role: "Front-end Developer",
-    email: "laurarossi@team.com",
-    img: "img/female1.png",
-  },
-  {
-    name: "Giorgio Verdi",
-    role: "Back-end Developer",
-    email: "giorgioverdi@team.com",
-    img: "img/male2.png",
-  },
-  {
-    name: "Marta Ipsum",
-    role: "SEO Specialist",
-    email: "martarossi@team.com",
-    img: "img/female2.png",
-  },
-  {
-    name: "Roberto Lorem",
-    role: "SEO Specialist",
-    email: "robertolorem@team.com",
-    img: "img/male3.png",
-  },
-  {
-    name: "Daniela Amet",
-    role: "Analyst",
-    email: "danielaamet@team.com",
-    img: "img/female3.png",
-  },
-];
-
-const divMain = document.getElementById("main-div");
-addCard(divMain);
+const container = document.getElementById("main-div");
+addCards(container);
 //
 const form = document.getElementById("form-input");
 form.addEventListener("submit", send);
